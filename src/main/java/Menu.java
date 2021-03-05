8e1225a55b5bc6808721bc77701513cd7d18b69a
Natalia <nataliafedorova1989@gmail.com>
2021-03-05 15:46:06 +0100

    added start method in Menu class which will print the app's welcoming  intro and options of the menu to choose from. Also added scanner java util package for the user's input later

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class Menu {
+import java.util.Scanner;
+
+public class Menu {
+
+    TaskList taskList;
+    Scanner scanner;
+
+    public Menu(){
+        this.taskList = new TaskList();
+    }
+
+    public void start() {
+        printIntro();
+        printOptions();
+    }
+
+
+    private void printIntro() {
+        System.out.println("__________________________________________________\n" +
+                "Hi there,\n" +
+                "\n" +
+                "Todo-list App is here to help you with your tasks! \n"+
+                "__________________________________________________\n"+
+
+                "Please choose an option:\n");
+    }
+
+    /**
+     * The app prints options
+     */
+    private void printOptions() {
+        System.out.println("(1) View Task List\n" +
+                "(2) Add New Task\n" +
+                "(3) Edit Task\n" +
+                "(4) Save and Quit\n");
+    }
+
 }
