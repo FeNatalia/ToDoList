f5e3c1890a5834822d680f69e740c48610a6b9ef
Natalia <nataliafedorova1989@gmail.com>
2021-03-07 15:45:49 +0100

    no changes here

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,6 +1,12 @@
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * TaskList class allows to keep a list of task.
+ * Serialization allows to convert an Object to byte stream
+ * and save it as file.
+ */
+
 public class TaskList implements Serializable {
     ArrayList<Task> tasks;
     public TaskList() {
