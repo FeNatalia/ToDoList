08dc0048ba6bc198804dcdcd6ab2b11552c34d03
Natalia <nataliafedorova1989@gmail.com>
2021-03-19 19:03:41 +0100

    edit Task function is added

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -9,13 +9,17 @@ import java.time.LocalDate;
 
 public class Menu {
 
+    FileSaver fileSaver;
     TaskList taskList;
     Scanner scanner;
+    TaskList completedTasks;
 
     public Menu(){
 
+        this.fileSaver= new FileSaver();
         this.taskList = new TaskList();
         this.scanner = new Scanner(System.in);
+        this.completedTasks = new TaskList();
     }
 
     /**
@@ -23,6 +27,8 @@ public class Menu {
      * and prints action options
      */
     public void start() {
+        this.taskList= fileSaver.viewSavedTask("myTasks.txt");
+        this.completedTasks = fileSaver.viewSavedTask("finishedTasks.txt");
         printIntro();
         printOptions();
         acceptOption();
@@ -48,7 +54,8 @@ public class Menu {
         System.out.println("(1) View Task List\n" +
                 "(2) Add New Task\n" +
                 "(3) Edit Task\n" +
-                "(4) Save and Quit");
+                "(4) Save and Quit\n"+
+                "(0) View finished tasks\n");
     }
 
     /**
@@ -86,11 +93,15 @@ public class Menu {
             case 2:
                 addTask();
                 break;
-
-           // case 3:
-
+            case 3:
+                editTask();
+                break;
             case 4:
+                saveTask();
                 quit = true;
+                break;
+            case 0:
+                viewTaskList(this.completedTasks);
                 break;
             default:
                 printWrongOptionWarning();
@@ -120,14 +131,17 @@ public class Menu {
         boolean correctCommand = false;
 
         while (!correctCommand) {
-            System.out.println("View Task List sorted by: \n(1) project name or \n(2) due date?" +
-                    "\nType 0 to go back to Main Menu");
+            System.out.println("View Task List sorted by: \n" +
+                    "(1) project name or \n" +
+                    "(2) due date?\n" +
+                    "Type 0 to go back to Main Menu");
             try {
                 int command = Integer.valueOf(scanner.nextLine());
                 switch (command) {
                     case 0:
                         printOptions();
                         acceptOption();
+                        break;
                     case 1:
                         taskList.sortByProject();
                         correctCommand = true;
@@ -151,13 +165,13 @@ public class Menu {
     public void addTask() {
         String title = getTaskTitle();
         String project = getProjectName();
-        LocalDate date = getDueDate();
+        LocalDate dueDate = getDueDate();
 
-        Task task = new Task (title, project, date);
+        Task task = new Task (title, project, dueDate);
 
         if (this.taskList.addTask(task)) {
             System.out.println("Your task \"" + title+ "\" is assigned to project \""
-                    + project + "\" with the due date on "+ getDueDate().toString());
+                    + project + "\" with the due date on "+ dueDate.toString());
         }
     }
 
@@ -166,7 +180,8 @@ public class Menu {
      * @return
      */
     private String getTaskTitle() {
-        String title;
+        String title="";
+
         System.out.println("Type your task's title:");
         title = scanner.nextLine();
         return title;
@@ -177,7 +192,7 @@ public class Menu {
      * @return
      */
     private String getProjectName() {
-        String project;
+        String project ="";
         System.out.println("Type a project name your task would belong to:");
         project = scanner.nextLine();
         return project;
@@ -188,13 +203,20 @@ public class Menu {
      */
     public LocalDate getDueDate() {
         LocalDate dueDate = null;
+
         while (dueDate == null) {
             System.out.println("When the task should get done?\n" +
                     "Please type in the following format: yyyy-mm-dd:");
             try {
                 LocalDate date = LocalDate.parse(scanner.nextLine());
-                dueDate = date;
+                LocalDate today = LocalDate.now();
+                if (date.compareTo(today) == -1) {
+                    System.out.println("You cannot choose the date which has passed already");
+                } else {
+                    dueDate = date;
+                }
             }
+
             catch (DateTimeParseException e) {
                 printWrongFormatWarning();
             }
@@ -213,6 +235,103 @@ public class Menu {
     private void printWrongFormatWarning() {
         System.out.println("Incorrect format, please follow this format: yyyy-mm-dd");
     }
+    public void saveTask() {
+        fileSaver.saveTask(this.taskList, "myTasks.txt");
+        fileSaver.saveTask(this.completedTasks, "finishedTasks.txt");
+    }
+
+    public void editTask(){
+        if (this.taskList.isEmpty()) {
+            System.out.println("You don't have any saved tasks in your list to edit.");
+            return;
+        }
+        int taskNum = pickTaskToEdit();
+        Task pickedTask = this.taskList.getTask(taskNum);
+
+        executeEditOption(pickedTask, taskNum);
+    }
+
+    public int pickTaskToEdit(){
+        while (true) {
+            System.out.println("Pick the task that you would like to edit:\n");
+            System.out.println(this.taskList.getTasks());
+
+            try {
+                int taskNum;
+                taskNum = Integer.valueOf(scanner.nextLine()) - 1;
+                this.taskList.getTask(taskNum);
+                return taskNum;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("\nPlease pick one existing task to edit\n");
+            } catch (IllegalArgumentException e) {
+                System.out.println("\nPlease pick one existing task to edit\n");
+            }
+        }
+    }
+
+    public void executeEditOption(Task pickedTask, int taskNum) {
+        boolean correctOption = false;
+        while (!correctOption) {
+            System.out.println("What would you like to edit?\n" +
+                    "(1) Task name\n" +
+                    "(2) Task project name\n" +
+                    "(3) Task deadline\n" +
+                    "OR you can as well:\n" +
+                    "(4) Mark Task as done\n" +
+                    "(5) Delete Task\n" +
+                    "(0) Go back to the main menu");
+            try {
+                int execute = Integer.valueOf(scanner.nextLine());
+
+                switch (execute) {
+                    case 1:
+                        String newTitle = getTaskTitle();
+                        pickedTask.setTitle(newTitle);
+                        System.out.println("The task name is edited to \'" + newTitle + "' now.\n");
+                        correctOption = true;
+                        break;
+                    case 2:
+                        String newProjectName = getProjectName();
+                        pickedTask.setProject(newProjectName);
+                        System.out.println("The project name is edited to \'" + newProjectName + "' now.\n");
+                        correctOption = true;
+                        break;
+                    case 3:
+                        LocalDate newDate = getDueDate();
+                        pickedTask.setDueDate(newDate);
+                        correctOption = true;
+                        break;
+                    case 4:
+                        pickedTask.markDone();
+                        if (this.taskList.removeTask(pickedTask) && this.taskList.addTask(pickedTask)) {
+                            System.out.println("The task is marked as done now and is transferred into completed task list \n" +
+                                    "which is available in the main menu");
+                            correctOption = true;
+                        } else {
+                            printWrongOptionWarning();
+                        }
+                        break;
+                    case 5:
+                        if (this.taskList.removeTask(pickedTask)) {
+                            System.out.println("The task is removed from your list now\n");
+                        } else {
+                            printWrongOptionWarning();
+                        }
+                        correctOption = true;
+                        break;
+                    case 0:
+                        printOptions();
+                        acceptOption();
+                    default:
+                        printWrongOptionWarning();
+                }
+            } catch (IllegalArgumentException e) {
+                printWrongOptionWarning();
+            }
+
+        }
+    }
+
 
 
 }
